65e0085badbd7bdf32808fc3cb2c5d1e88093bb7
orrin <orrin.rhodes@outlook.com>
2021-05-19 22:58:01 -0400

    Update ripple.js

--- js/ripple.js
+++ js/ripple.js
@@ -5,7 +5,7 @@ function sleep(ms) {
 };
 
 //Ripple Event Handler
-var drawRipple = async function(ev) {
+async function drawRipple(ev) {
     var x = ev.clientX;
     var y = ev.clientY;
     var node = document.querySelector(".ripple");
@@ -18,26 +18,10 @@ var drawRipple = async function(ev) {
     clone.remove();
 };
 
-//Ripple Triggers
-async function wait(x){
-    i=1;
-    while (true) {
-        i++;
-        await sleep(x);
-        console.log(i)
-        if(i % 2 == 0) {
-            console.log('yea')
-            
-        };
-    };
-};
-
-
-wait(500);
 window.addEventListener("mousemove", drawRipple);
 
 //Control Handler
-var controlHandler = function() {
+function controlHandler() {
   document.body.classList.toggle("dark");
   if (document.body.classList.contains("dark")) {
     controller.textContent = "Dark Ripple";
